78ba81da3fa9dda15052efb763403f3d2115049f
Joanna Michalczewska <joanna.michalczewska@yahoo.com>
2021-12-24 10:09:27 +0100

    Implement BlocProvider and BlocBuilder

--- lib/app/home/restaurants/restaurants_page_content.dart
+++ lib/app/home/restaurants/restaurants_page_content.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:najlepsza_pizza_w_gdyni/app/home/restaurants/cubit/restaurants_cubit.dart';
 
 class RestaurantsPageContent extends StatelessWidget {
   const RestaurantsPageContent({
@@ -8,54 +10,61 @@ class RestaurantsPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('restaurants')
-            .orderBy('rating', descending: true)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
-          }
+    return BlocProvider(
+      create: (context) => RestaurantsCubit(),
+      child: BlocBuilder<RestaurantsCubit, RestaurantsState>(
+        builder: (context, state) {
+          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('restaurants')
+                  .orderBy('rating', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Center(child: Text('Something went wrong'));
+                }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text("Loading"));
-          }
-          final documents = snapshot.data!.docs;
-          return Container(
-            color: Colors.grey,
-            child: ListView(
-              children: [
-                for (final document in documents) ...[
-                  Dismissible(
-                    key: ValueKey(document.id),
-                    onDismissed: (_) {
-                      FirebaseFirestore.instance
-                          .collection('restaurants')
-                          .doc(document.id)
-                          .delete();
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(document['name']),
-                              Text(document['pizza']),
-                            ],
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text("Loading"));
+                }
+                final documents = snapshot.data!.docs;
+                return Container(
+                  color: Colors.grey,
+                  child: ListView(
+                    children: [
+                      for (final document in documents) ...[
+                        Dismissible(
+                          key: ValueKey(document.id),
+                          onDismissed: (_) {
+                            FirebaseFirestore.instance
+                                .collection('restaurants')
+                                .doc(document.id)
+                                .delete();
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(document['name']),
+                                    Text(document['pizza']),
+                                  ],
+                                ),
+                                Text(document['rating'].toString()),
+                              ],
+                            ),
                           ),
-                          Text(document['rating'].toString()),
-                        ],
-                      ),
-                    ),
+                        ),
+                      ],
+                    ],
                   ),
-                ],
-              ],
-            ),
-          );
-        });
+                );
+              });
+        },
+      ),
+    );
   }
 }
