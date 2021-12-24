4666057b22a43fc860bc71c556c2e3d8a442661b
Joanna Michalczewska <joanna.michalczewska@yahoo.com>
2021-12-24 09:26:55 +0100

    Add new class

--- lib/app/home/restaurants/cubit/restaurants_state.dart
+++ lib/app/home/restaurants/cubit/restaurants_state.dart
@@ -1,4 +1,8 @@
 part of 'restaurants_cubit.dart';
 
 @immutable
-class RestaurantsState {}
+class RestaurantsState {
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> documents;
+
+  RestaurantsState({required this.documents});
+}
